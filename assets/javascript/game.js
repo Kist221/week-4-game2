6e7477b11ecc1d74ea7ce488327358a98ee46938
Kist221 <Kist221@gmail.com>
2017-11-17 21:28:04 -0700

    images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,42 @@
 
+// characters
+var c3po = {
+	name: "one",
+	health: 100,
+	attack: 5,
+	counter: 10,
+	chosen: false
+};
+var r2d2 = {
+	name: "two",
+	health: 100,
+	attack: 5,
+	counter: 10,
+	chosen: false
+};
+var bb8 = {
+	name: "three",
+	health: 100,
+	attack: 5,
+	counter: 10,
+	chosen: false
+};
+var jarjar = {
+	name: "four",
+	health: 100,
+	attack: 5,
+	counter: 10,
+	chosen: false
+};
 
 
 
 
+
+// functions
+
+var choose = function(this) {
+	// body...
+	
+}
 
