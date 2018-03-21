7d5a168f9c9bb9f986ab65ef55372ccb4794bf8b
Kist221 <Kist221@gmail.com>
2018-03-20 20:18:07 -0700

    tracking

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,64 @@
 
-// characters
-var c3po = {
-	name: "one",
-	health: 100,
-	attack: 5,
-	counter: 10,
-	chosen: false
-};
-var r2d2 = {
-	name: "two",
-	health: 100,
-	attack: 5,
-	counter: 10,
-	chosen: false
-};
-var bb8 = {
-	name: "three",
-	health: 100,
-	attack: 5,
-	counter: 10,
-	chosen: false
-};
-var jarjar = {
-	name: "four",
-	health: 100,
-	attack: 5,
-	counter: 10,
-	chosen: false
-};
+// global variables
+var hero;
+var enemies;
+var enemy;
+var heroChosen = false;
+var enemyChosen = false;
 
 
-
-
-
-// functions
-
-var choose = function(this) {
-	// body...
-	
+// game functions
+var heroSelect = function () 
+{
+	// choose hero on click
+	$("#picksBox").on("click", ".pick", function()
+	{
+		if (heroChosen === false) 
+		{
+			// get hero HTML and append to #Hero box
+			hero = $(this);
+			$("#heroBox").append(hero);
+			heroChosen = true;
+			// runs function to populate enemies and clear choice box
+			popEnemies();
+			$("#picksBox").empty();
+		} 
+	});
 }
+
+// move unchosen heroes to enemies box
+var popEnemies = function () 
+{
+	// if hero chosen, move all else to #Enemies box
+	enemies = $("#picksBox").html();
+	$("#enemyBox").append(enemies);
+}
+
+var enemySelect = function ()
+{
+	// choose enemy on click
+	$("#enemyBox").on("click", ".pick", function()
+	{
+		if (enemyChosen === false) 
+		{
+			// get enemy HTML and append to #enemy box
+			enemy = $(this);
+			$("#defenderBox").append(enemy);
+			enemyChosen = true;
+		} 
+		else
+		{
+			// replace defender with new selection
+			$("#enemyBox").append(enemy);
+			enemy = $(this);
+			$("#defenderBox").append(enemy);
+		}
+	});
+}
+
+
+
+
+heroSelect();
+enemySelect();
 
